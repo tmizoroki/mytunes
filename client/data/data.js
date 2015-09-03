61f0f7a979ae21f86c84ad7b001547dae6a0c773
tmizoroki <tmizoroki@gmail.com>
2015-09-02 21:05:45 -0700

    Implemented dequeue function added listener

--- client/data/data.js
+++ client/data/data.js
@@ -21,3 +21,38 @@ var songData = [
     artist: "Aaliyah",
   }
 ];
+
+
+// 0) (index.html)Instantiate appModel.js-- pass in a collection of song objects. It has a listener
+// on each song object's "play" method being invoked
+
+// 1) (index.html)Connect appView to the appModel instance
+
+// 2) (index.html)"draw" the initial appView
+
+// ...
+
+
+// 1)) LibraryEntryView: click event on song instance. Invoke play on the song instance
+
+// 2) SongModel.js: has a play method. But it doesn't do anything. It just triggers
+//  play: function() {
+//     // Triggering an event here will also trigger the event on the collection
+//     this.trigger('play', this);
+//   },
+
+// 3) AppModel.js has a listener on every song model instnace. When play "triggers" it sets ITS OWN (appmodel's)
+// currentSong property the song instance
+
+// 4) AppView.js: There's a listener on its associated APP MODEL. (NOT THE SONG!). The listener
+// listens for changes on APP MODEL'S currentSong changing. If it changes it retreives the currentSong
+// from appModel and then invokes playView's setSong() method, passing in the song model instance
+
+// 5) PlayerView.js: It has a setSong method and the song was passed through. It sets PlayerView's (it's own)
+// model property to the song instance and then it calls its render function
+
+// 5a) PlayerView.js its render function sets the "SRC" attribute on the <AUDIO> element. and WhenSRC gets
+// set on an AUDIO element. It starts playing.
+// render: function() {
+//     return this.$el.attr('src', this.model ? this.model.get('url') : '');
+//   }
--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -2,9 +2,11 @@
 var AppModel = Backbone.Model.extend({
 
   initialize: function(params) {
+    console.log("app model initialized!");
+    console.log(params.library.models);
+
     this.set('currentSong', new SongModel());
     this.set('songQueue', new SongQueue());
-
     /* Note that 'this' is passed as the third argument. That third argument is
     the context. The 'play' handler will always be bound to that context we pass in.
     In this example, we're binding it to the App. This is helpful because otherwise
@@ -12,10 +14,25 @@ var AppModel = Backbone.Model.extend({
     end up referring to the window. That's just what happens with all JS events. The handlers end up
     getting called from the window (unless we override it, as we do here). */
 
+    params.library.on('enqueue', function(song) {
+      console.log("From appmodel: detected enqueue");
+      var ref = this.get("songQueue");
+      ref.add(song);
+    }, this);
+
 
     params.library.on('play', function(song) {
       this.set('currentSong', song);
     }, this);
+  },
+
+  dequeue: function(){
+      var songQueueRef = this.get("songQueue");
+
+      songQueueRef.remove(songQueueRef.at(0));
+        //We think it'll re-order [it did.]
+
+      songQueueRef.playFirst()
   }
 
 });
