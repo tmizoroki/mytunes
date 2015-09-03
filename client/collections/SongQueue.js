3a9791198baba1459dfc761a9bb63fb7f205d917
tmizoroki <tmizoroki@gmail.com>
2015-09-02 21:04:42 -0700

    Added model, playFirst() implemented

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,16 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
+  model: SongModel,
+
+
   initialize: function() {
+    
+  },
+
+  playFirst: function() {
+    //We should do an undefined check here too
+    this.at(0).play();
   }
 
 });
