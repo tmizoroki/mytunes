d69770618fbdfb579006fede96d49d254af0c458
tmizoroki <tmizoroki@gmail.com>
2015-09-03 15:49:03 -0700

    Added rendering hooks for SongQueueEntryView

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,4 +1,21 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
+  //model: SongModel,
   // your code here!
+
+  tagname: 'tr', //Tomio
+
+  template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'), //Tomio
+
+  events: { //Tomio
+    'click': function() {
+      //Revisit
+      this.model.removeSong();
+    }
+  },
+
+  render: function() {
+    //this.trigger("render", this);
+    return this.$el.html(this.template(this.model.attributes));
+  }
 });
