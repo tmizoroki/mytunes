fa0d0c89929489342acb95b399bc6634c781b464
tmizoroki <tmizoroki@gmail.com>
2015-09-02 21:07:48 -0700

    Added songOver emitter

--- client/views/PlayerView.js
+++ client/views/PlayerView.js
@@ -1,6 +1,6 @@
 // PlayerView.js - Defines a backbone view class for the music player.
 var PlayerView = Backbone.View.extend({
-
+  
   // HTML5 (native) audio tag is being used
   // see: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML5_audio_and_video
   el: '<audio id="track" controls autoplay />',
@@ -8,8 +8,22 @@ var PlayerView = Backbone.View.extend({
 
   events:{
     "ended": function(){
-      alert('something ended');
-      console.log("somethign ended")
+      console.log("From PlayerView: something ended");
+      //We need a reference to songQueue here
+
+      //Call appModel.dequeue()
+        //COme up with some way to reference it HERE
+        //Have a listener on 
+        // this.trigger("ended", this);
+        this.songOver();
+          // var songQueueRef = "something";
+
+          // songQueueRef.remove(songQueueRef.at(0));
+          //   //We think it'll re-order
+
+          // songQueueRef.playFirst()
+          // //Remove the song at zero
+          //   //hit playFirst()
     }
   },
   initialize: function() {
@@ -22,6 +36,10 @@ var PlayerView = Backbone.View.extend({
 
   render: function() {
     return this.$el.attr('src', this.model ? this.model.get('url') : '');
+  },
+
+  songOver: function(){
+    this.trigger("songOver", this);
   }
 
 });
