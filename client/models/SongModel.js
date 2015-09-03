cdbd6a00bb17fc18a6647c5fe76bb2539c0d4de4
tmizoroki <tmizoroki@gmail.com>
2015-09-02 21:06:16 -0700

    Implemented enqueue

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -4,6 +4,13 @@ var SongModel = Backbone.Model.extend({
   play: function() {
     // Triggering an event here will also trigger the event on the collection
     this.trigger('play', this);
+  },
+
+  enqueue: function(){
+    console.log("From SongModel: enquue actually fired")
+    //Add this instance to SongQueue
+    this.trigger('enqueue', this);
+
   }
 
 });
